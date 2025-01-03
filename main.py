101ce6bb971c35f369f5340a0c5e33907c903403
Leo Gimenez <leogimenez.dev@gmail.com>
2025-01-03 12:45:58 -0300

    se trabaja en datos de qr

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from barcode import Code128
 from barcode.writer import ImageWriter
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
+
+# Crear un código QR
+qr_data = "https://www.example.com"
